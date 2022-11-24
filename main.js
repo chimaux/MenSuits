e3b1ef35aa88ba44ef70626018259cb08d0f9b02
Chima <ugoppromise@gmail.com>
2022-11-25 00:50:11 +0100

    major fix

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-//alert('workin')
+// alert(document.body.clientHeight)
+
+const myHeader = document.querySelector('header')
+let newcolor = document.querySelector("#down")
+
+window.onscroll = function() {
+//   if(window.scrollY > window.innerHeight){
+  if(window.scrollY > 399.7){
+    myHeader.classList.add('scrolled')
+    newcolor.style.color="black";
+  }else{
+    myHeader.classList.remove('scrolled')
+    newcolor.style.color="white";
+  }
+}
